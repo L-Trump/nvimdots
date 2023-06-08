ed066fd5660b147a0c959bfc68d1e4dfae09f0e6
L-Trump <yinghaochi@163.com>
2023-06-08 22:09:56 +0800

    add wrapping.nvim for latex soft wrapping

--- lua/modules/plugins/custom.lua
+++ lua/modules/plugins/custom.lua
@@ -7,4 +7,20 @@ custom['lervag/vimtex'] = {
     config = require("custom.vimtex"),
 }
 
+custom['andrewferrier/wrapping.nvim'] = {
+    lazy = true,
+    ft = {
+        "asciidoc",
+        "gitcommit",
+        "latex",
+        "mail",
+        "markdown",
+        "rst",
+        "tex",
+        "text",
+    },
+    config = require("custom.wrapping"),
+	event = "BufWinEnter",
+}
+
 return custom
